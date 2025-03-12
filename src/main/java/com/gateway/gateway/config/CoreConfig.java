ef196151c4b9ff876e60684e46690dca320eac74
jishnuAVijayan012 <jishnu.vijayan@seeroo.com>
2025-03-12 20:26:26 +0530

    new commit gateway

---
+++ src/main/java/com/gateway/gateway/config/CoreConfig.java
@@ -0,0 +1,32 @@
+package com.gateway.gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CoreConfig {
+
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin(allowedOrigins); // React frontend
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*"); // Allow all methods (GET, POST, etc.)
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+
+    }
+
+}
